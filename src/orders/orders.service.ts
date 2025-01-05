38687dd7a674c12cf0e7815137c5b03d0b14405f
sebasGZA <sebassga34@gmail.com>
2025-01-04 19:32:08 -0500

    feat: connect orders ms and products ms

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,27 +1,86 @@
-import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import {
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { OrderItem, PrismaClient } from '@prisma/client';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+
 import { CreateOrderDto } from './dto/create-order.dto';
-import { PrismaClient } from '@prisma/client';
-import { RpcException } from '@nestjs/microservices';
 import { OrderPaginationDto } from './dto/order-pagination';
 import { OrderChangeStatusDto } from './dto/order-change-status.dto';
+import { PRODUCTS_SERVICE } from '../config';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger(OrdersService.name);
+
+  constructor(
+    @Inject(PRODUCTS_SERVICE)
+    private readonly productsClient: ClientProxy,
+  ) {
+    super();
+  }
 
   async onModuleInit() {
     await this.$connect();
     this.logger.log('Database connected');
   }
 
-  create(createOrderDto: CreateOrderDto) {
-    return {
-      service: 'Orders Microservice',
-      createOrderDto,
-    };
-    // return this.order.create({
-    //   data: createOrderDto,
-    // });
+  async create({ items }: CreateOrderDto) {
+    try {
+      const ids = items.map((item) => item.productId);
+      const products: any[] = await firstValueFrom(
+        this.productsClient.send(
+          {
+            cmd: 'validate_products',
+          },
+          ids,
+        ),
+      );
+
+      const totalAmount: number = items.reduce((_, orderItem: OrderItem) => {
+        const price: number = products.find(
+          (product) => product.id === orderItem.productId,
+        ).price;
+
+        return price * orderItem.quantity;
+      }, 0);
+
+      const totalItems = items.reduce((acc, orderItem) => {
+        return acc + orderItem.quantity;
+      }, 0);
+
+      const order = await this.order.create({
+        data: {
+          totalAmount,
+          totalItems,
+          orderItem: {
+            createMany: {
+              data: items.map(({ productId, quantity }: OrderItem) => ({
+                price: products.find((product) => product.id === productId)
+                  .price,
+                productId,
+                quantity,
+              })),
+            },
+          },
+        },
+        include: {
+          orderItem: true,
+        },
+      });
+
+      return order;
+    } catch {
+      throw new RpcException({
+        sttus: HttpStatus.BAD_REQUEST,
+        message: 'Chech logs',
+      });
+    }
   }
 
   async findAll(paginationDto: OrderPaginationDto) {
